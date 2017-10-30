d1c734bb5491943c65e33c4fb7c25bd66b0fec94
SunoAries <476252630@qq.com>
2017-10-30 10:05:20 +0800

    fukc

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,8 @@ const hub = new HubRegistry([conf.path.tasks('*.js')]);
 
 // Tell gulp to use the tasks just loaded
 gulp.registry(hub);
-
+console.log('不需要被revert的代码')
+console.log('不需要被revert的代码')
 gulp.task('build', gulp.series(gulp.parallel('other', 'webpack:dist')));
 gulp.task('test', gulp.series('karma:single-run'));
 gulp.task('test:auto', gulp.series('karma:auto-run'));
